51667f5757920f259777246c9494e8c2d627631a
underscore830 <70597619+ldiamond830@users.noreply.github.com>
2024-01-19 11:36:54 -0500

    adding DX12 start

---
+++ DX12Starter-master/Game.cpp
@@ -0,0 +1,348 @@
+#include "Game.h"
+#include "Vertex.h"
+#include "DX12Helper.h"
+#include "Input.h"
+#include "PathHelpers.h"
+
+// Needed for a helper function to load pre-compiled shader files
+#pragma comment(lib, "d3dcompiler.lib")
+#include <d3dcompiler.h>
+
+// For the DirectX Math library
+using namespace DirectX;
+
+// --------------------------------------------------------
+// Constructor
+//
+// DXCore (base class) constructor will set up underlying fields.
+// Direct3D itself, and our window, are not ready at this point!
+//
+// hInstance - the application's OS-level handle (unique ID)
+// --------------------------------------------------------
+Game::Game(HINSTANCE hInstance)
+	: DXCore(
+		hInstance,			// The application's handle
+		L"DirectX Game",	// Text for the window's title bar (as a wide-character string)
+		1280,				// Width of the window's client area
+		720,				// Height of the window's client area
+		false,				// Sync the framerate to the monitor refresh? (lock framerate)
+		true)				// Show extra stats (fps) in title bar?
+{
+#if defined(DEBUG) || defined(_DEBUG)
+	// Do we want a console window?  Probably only in debug mode
+	CreateConsoleWindow(500, 120, 32, 120);
+	printf("Console window created successfully.  Feel free to printf() here.\n");
+#endif
+
+	ibView = {};
+	vbView = {};
+}
+
+// --------------------------------------------------------
+// Destructor - Clean up anything our game has created:
+//  - Delete all objects manually created within this class
+//  - Release() all Direct3D objects created within this class
+// --------------------------------------------------------
+Game::~Game()
+{
+	// We need to wait here until the GPU
+	// is actually done with its work
+	DX12Helper::GetInstance().WaitForGPU();
+}
+
+// --------------------------------------------------------
+// Called once per program, after Direct3D and the window
+// are initialized but before the game loop.
+// --------------------------------------------------------
+void Game::Init()
+{
+	// Helper methods for loading shaders, creating some basic
+	// geometry to draw and some simple camera matrices.
+	//  - You'll be expanding and/or replacing these later
+	CreateRootSigAndPipelineState();
+	CreateGeometry();
+}
+
+// --------------------------------------------------------
+// Loads the two basic shaders, then creates the root signature 
+// and pipeline state object for our very basic demo.
+// --------------------------------------------------------
+void Game::CreateRootSigAndPipelineState()
+{
+	// Blobs to hold raw shader byte code used in several steps below
+	Microsoft::WRL::ComPtr<ID3DBlob> vertexShaderByteCode;
+	Microsoft::WRL::ComPtr<ID3DBlob> pixelShaderByteCode;
+
+	// Load shaders
+	{
+		// Read our compiled vertex shader code into a blob
+		// - Essentially just "open the file and plop its contents here"
+		D3DReadFileToBlob(FixPath(L"VertexShader.cso").c_str(), vertexShaderByteCode.GetAddressOf());
+		D3DReadFileToBlob(FixPath(L"PixelShader.cso").c_str(), pixelShaderByteCode.GetAddressOf());
+	}
+
+	// Input layout
+	const unsigned int inputElementCount = 2;
+	D3D12_INPUT_ELEMENT_DESC inputElements[inputElementCount] = {};
+	{
+		// Create an input layout that describes the vertex format
+		// used by the vertex shader we're using
+		//  - This is used by the pipeline to know how to interpret the raw data
+		//     sitting inside a vertex buffer
+
+		// Set up the first element - a position, which is 3 float values
+		inputElements[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+		inputElements[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+		inputElements[0].SemanticName = "POSITION";
+		inputElements[0].SemanticIndex = 0;
+
+		// Set up the second element - a color, which is 4 more float values
+		inputElements[1].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+		inputElements[1].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+		inputElements[1].SemanticName = "COLOR";
+		inputElements[1].SemanticIndex = 0;
+	}
+
+	// Root Signature
+	{
+		// Describe and serialize the root signature
+		D3D12_ROOT_SIGNATURE_DESC rootSig = {};
+		rootSig.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+		rootSig.NumParameters = 0;
+		rootSig.pParameters = 0;
+		rootSig.NumStaticSamplers = 0;
+		rootSig.pStaticSamplers = 0;
+
+		ID3DBlob* serializedRootSig = 0;
+		ID3DBlob* errors = 0;
+
+		D3D12SerializeRootSignature(
+			&rootSig,
+			D3D_ROOT_SIGNATURE_VERSION_1,
+			&serializedRootSig,
+			&errors);
+
+		// Check for errors during serialization
+		if (errors != 0)
+		{
+			OutputDebugString((wchar_t*)errors->GetBufferPointer());
+		}
+
+		// Actually create the root sig
+		device->CreateRootSignature(
+			0,
+			serializedRootSig->GetBufferPointer(),
+			serializedRootSig->GetBufferSize(),
+			IID_PPV_ARGS(rootSignature.GetAddressOf()));
+	}
+
+	// Pipeline state
+	{
+		// Describe the pipeline state
+		D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc = {};
+
+		// -- Input assembler related ---
+		psoDesc.InputLayout.NumElements = inputElementCount;
+		psoDesc.InputLayout.pInputElementDescs = inputElements;
+		psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+
+		// Root sig
+		psoDesc.pRootSignature = rootSignature.Get();
+
+		// -- Shaders (VS/PS) --- 
+		psoDesc.VS.pShaderBytecode = vertexShaderByteCode->GetBufferPointer();
+		psoDesc.VS.BytecodeLength = vertexShaderByteCode->GetBufferSize();
+		psoDesc.PS.pShaderBytecode = pixelShaderByteCode->GetBufferPointer();
+		psoDesc.PS.BytecodeLength = pixelShaderByteCode->GetBufferSize();
+
+		// -- Render targets ---
+		psoDesc.NumRenderTargets = 1;
+		psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+		psoDesc.DSVFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+		psoDesc.SampleDesc.Count = 1;
+		psoDesc.SampleDesc.Quality = 0;
+
+		// -- States ---
+		psoDesc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID;
+		psoDesc.RasterizerState.CullMode = D3D12_CULL_MODE_BACK;
+		psoDesc.RasterizerState.DepthClipEnable = true;
+
+		psoDesc.DepthStencilState.DepthEnable = true;
+		psoDesc.DepthStencilState.DepthFunc = D3D12_COMPARISON_FUNC_LESS;
+		psoDesc.DepthStencilState.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
+
+		psoDesc.BlendState.RenderTarget[0].SrcBlend = D3D12_BLEND_ONE;
+		psoDesc.BlendState.RenderTarget[0].DestBlend = D3D12_BLEND_ZERO;
+		psoDesc.BlendState.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
+		psoDesc.BlendState.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+
+		// -- Misc ---
+		psoDesc.SampleMask = 0xffffffff;
+
+		// Create the pipe state object
+		device->CreateGraphicsPipelineState(&psoDesc,
+			IID_PPV_ARGS(pipelineState.GetAddressOf()));
+	}
+}
+
+// --------------------------------------------------------
+// Creates the geometry we're going to draw - a single triangle for now
+// --------------------------------------------------------
+void Game::CreateGeometry()
+{
+	// Create some temporary variables to represent colors
+	// - Not necessary, just makes things more readable
+	XMFLOAT4 red	= XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
+	XMFLOAT4 green	= XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f);
+	XMFLOAT4 blue	= XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f);
+
+	// Set up the vertices of the triangle we would like to draw
+	// - We're going to copy this array, exactly as it exists in CPU memory
+	//    over to a Direct3D-controlled data structure on the GPU (the vertex buffer)
+	// - Note: Since we don't have a camera or really any concept of
+	//    a "3d world" yet, we're simply describing positions within the
+	//    bounds of how the rasterizer sees our screen: [-1 to +1] on X and Y
+	// - This means (0,0) is at the very center of the screen.
+	// - These are known as "Normalized Device Coordinates" or "Homogeneous 
+	//    Screen Coords", which are ways to describe a position without
+	//    knowing the exact size (in pixels) of the image/window/etc.  
+	// - Long story short: Resizing the window also resizes the triangle,
+	//    since we're describing the triangle in terms of the window itself
+	Vertex vertices[] =
+	{
+		{ XMFLOAT3(+0.0f, +0.5f, +0.0f), red },
+		{ XMFLOAT3(+0.5f, -0.5f, +0.0f), blue },
+		{ XMFLOAT3(-0.5f, -0.5f, +0.0f), green },
+	};
+
+	// Set up indices, which tell us which vertices to use and in which order
+	// - This is redundant for just 3 vertices, but will be more useful later
+	// - Indices are technically not required if the vertices are in the buffer 
+	//    in the correct order and each one will be used exactly once
+	// - But just to see how it's done...
+	unsigned int indices[] = { 0, 1, 2 };
+
+
+	// Create the two buffers
+	DX12Helper& dx12Helper = DX12Helper::GetInstance();
+	vertexBuffer = dx12Helper.CreateStaticBuffer(sizeof(Vertex), ARRAYSIZE(vertices), vertices);
+	indexBuffer = dx12Helper.CreateStaticBuffer(sizeof(unsigned int), ARRAYSIZE(indices), indices);
+
+	// Set up the views
+	vbView.StrideInBytes = sizeof(Vertex);
+	vbView.SizeInBytes = sizeof(Vertex) * ARRAYSIZE(vertices);
+	vbView.BufferLocation = vertexBuffer->GetGPUVirtualAddress();
+
+	ibView.Format = DXGI_FORMAT_R32_UINT;
+	ibView.SizeInBytes = sizeof(unsigned int) * ARRAYSIZE(indices);
+	ibView.BufferLocation = indexBuffer->GetGPUVirtualAddress();
+}
+
+
+// --------------------------------------------------------
+// Handle resizing to match the new window size.
+//  - DXCore needs to resize the back buffer
+//  - Eventually, we'll want to update our 3D camera
+// --------------------------------------------------------
+void Game::OnResize()
+{
+	// Handle base-level DX resize stuff
+	DXCore::OnResize();
+}
+
+// --------------------------------------------------------
+// Update your game here - user input, move objects, AI, etc.
+// --------------------------------------------------------
+void Game::Update(float deltaTime, float totalTime)
+{
+	// Example input checking: Quit if the escape key is pressed
+	if (Input::GetInstance().KeyDown(VK_ESCAPE))
+		Quit();
+}
+
+// --------------------------------------------------------
+// Clear the screen, redraw everything, present to the user
+// --------------------------------------------------------
+void Game::Draw(float deltaTime, float totalTime)
+{
+	// Grab the current back buffer for this frame
+	Microsoft::WRL::ComPtr<ID3D12Resource> currentBackBuffer = backBuffers[currentSwapBuffer];
+
+	// Clearing the render target
+	{
+		// Transition the back buffer from present to render target
+		D3D12_RESOURCE_BARRIER rb = {};
+		rb.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+		rb.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+		rb.Transition.pResource = currentBackBuffer.Get();
+		rb.Transition.StateBefore = D3D12_RESOURCE_STATE_PRESENT;
+		rb.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
+		rb.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+		commandList->ResourceBarrier(1, &rb);
+
+		// Background color (Cornflower Blue in this case) for clearing
+		float color[] = { 0.4f, 0.6f, 0.75f, 1.0f };
+
+		// Clear the RTV
+		commandList->ClearRenderTargetView(
+			rtvHandles[currentSwapBuffer],
+			color,
+			0, 0); // No scissor rectangles
+
+		// Clear the depth buffer, too
+		commandList->ClearDepthStencilView(
+			dsvHandle,
+			D3D12_CLEAR_FLAG_DEPTH,
+			1.0f,	// Max depth = 1.0f
+			0,		// Not clearing stencil, but need a value
+			0, 0);	// No scissor rects
+	}
+
+	// Rendering here!
+	{
+		// Set overall pipeline state
+		commandList->SetPipelineState(pipelineState.Get());
+
+		// Root sig (must happen before root descriptor table)
+		commandList->SetGraphicsRootSignature(rootSignature.Get());
+
+		// Set up other commands for rendering
+		commandList->OMSetRenderTargets(1, &rtvHandles[currentSwapBuffer], true, &dsvHandle);
+		commandList->RSSetViewports(1, &viewport);
+		commandList->RSSetScissorRects(1, &scissorRect);
+		commandList->IASetVertexBuffers(0, 1, &vbView);
+		commandList->IASetIndexBuffer(&ibView);
+		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+		// Draw
+		commandList->DrawIndexedInstanced(3, 1, 0, 0, 0);
+	}
+
+	// Present
+	{
+		// Transition back to present
+		D3D12_RESOURCE_BARRIER rb = {};
+		rb.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+		rb.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+		rb.Transition.pResource = currentBackBuffer.Get();
+		rb.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
+		rb.Transition.StateAfter = D3D12_RESOURCE_STATE_PRESENT;
+		rb.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+		commandList->ResourceBarrier(1, &rb);
+
+		// Must occur BEFORE present
+		DX12Helper::GetInstance().CloseExecuteAndResetCommandList();
+
+		// Present the current back buffer
+		bool vsyncNecessary = vsync || !deviceSupportsTearing || isFullscreen;
+		swapChain->Present(
+			vsyncNecessary ? 1 : 0,
+			vsyncNecessary ? 0 : DXGI_PRESENT_ALLOW_TEARING);
+
+		// Figure out which buffer is next
+		currentSwapBuffer++;
+		if (currentSwapBuffer >= numBackBuffers)
+			currentSwapBuffer = 0;
+	}
+
+}
